7c147e3035089ecaf5d11c70992b2dd02a69ff0f
Kenny Crump <kennycrump@Kennys-MBP.home>
2018-11-11 15:34:31 -0700

    counter and select

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,38 @@
     <input v-model.number="num2" type="number" />
     <span>=</span>
     <span>{{num1 + num2}}</span>
+    <br>
+    <select name="food" v-model='name'>
+      <option>taco</option>
+      <option>burrito</option>
+    </select>
+    <span v-if="name">My favorite food is {{name}}</span>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+// new Vue({
+//   el: '#app',
+//   data(){
+//     return{
+//       num1: 0,
+//       num2: 0,
+//       name: ''
+//     }
+//   }
+// })
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      num1: 0,
+      num2: 0,
+      name: ''
+    }
   }
 }
 </script>
